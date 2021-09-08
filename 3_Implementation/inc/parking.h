ebcd4cbd10ae62ddd5d07150bbc43febf3bbe662
TupaakulaMonica <89584933+TupaakulaMonica@users.noreply.github.com>
2021-09-08 20:40:46 +0530

    Update and rename calculator_operations.h to parking.h

---
+++ 3_Implementation/inc/parking.h
@@ -0,0 +1,14 @@
+#ifndef PARKING_H
+#define PARKING_H
+
+#include "stdio.h"
+#include "conio.h"
+
+void truck( );
+void car( );
+void rikshaw( );
+void bike( );
+void status( );
+void clear( );
+
+#endif
